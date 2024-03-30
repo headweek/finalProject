44a58eaf5312b493157957524024ac321a329c32
headweek <abdullayev233@gmail.com>
2024-03-30 04:13:32 +0400

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Salman Abdullayev on 30.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
